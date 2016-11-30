1f3b43d9441925eced3dd1eab22236644c709cc2
SampaioFelipe <sampaio.motion@gmail.com>
2016-11-30 15:17:39 -0200

    teste de cópia de arquivos

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <stdio.h>
+#include <fstream>
+
 #include <string.h>
+
 #include <sys/stat.h> // Para a criação de diretórios
 #include <dirent.h> // directory header
 
@@ -10,19 +12,50 @@ using namespace std;
 
 int arquivar(char* dir, int nivel)
 {
+    streampos size;
+    char * memblock;
+    /**Abertura de novo Arquivo**/
+    ofstream arquivo;
+    ifstream imagem;
+
+    arquivo.open("nome.png", ios::binary|ios::trunc); // Temos que verificar se já não exite arquivo com o mesmo nome (?)
+    imagem.open("ghibli.png", ios::binary);
+
+    //size = imagem.tellg();
+    //cout<<size<<endl;
+    //memblock = new char [size];
+    //imagem.read(memblock, size);
+    //arquivo.write(memblock, size);
+
+    if(imagem.is_open() && arquivo.is_open())
+	{
+		while(!imagem.eof())
+		{
+			arquivo.put(imagem.get());
+		}
+	}
+
+    imagem.close();
+    arquivo.close();
+
+    /**Parte dos diretórios**/
+
     DIR* ptrDir = NULL; // Ponteiro para o diretório (DIR representa um stream de diretório)
 
     struct dirent* ptrEnt = NULL; // Usado para armazenar ifomrações sobre o diretório
 
     ptrDir = opendir(dir); // abre e retorna um directory stream
 
-    if(ptrDir == NULL){
+    if(ptrDir == NULL)
+    {
         cout<<"ERRO: diretorio não encontrado"<<endl;
         return 1;
     }
 
-    while(ptrEnt = readdir(ptrDir)){ // readdir lê a próxima entrada do diretório (retorna um struct dirent*)
-        if(ptrEnt == NULL){
+    while(ptrEnt = readdir(ptrDir))  // readdir lê a próxima entrada do diretório (retorna um struct dirent*)
+    {
+        if(ptrEnt == NULL)
+        {
             cout<<"ERRO ptrEnt"<<endl;
             return 1;
         }
@@ -61,31 +94,33 @@ int extrair(char* arquivo)
     }
 }
 
+void info()
+{
+    cout<<"[Descrição do programa]"<<endl;
+}
+
 int main(int argc, char* argv[])
 {
     int status = 0;
 
     if(argc != 3)
     {
-        cout<<"Formato Incorreto: Descrição do programa"<<endl;
+        info();
+        status = 3;
     }
     else
     {
-        cout<<"Formato Correto\nArgumentos lidos: "<<argv[1]<<" "<<argv[2]<<endl;
-        switch(argv[1][1])
-        {
-        case 'c':
+        string op = argv[1];
+        if(op.compare("-c") == 0)
             status = arquivar(argv[2], 0);
-            break;
-        case 'l':
+        else if(op.compare("-l") == 0)
             cout<<"Listar"<<endl;
-            break;
-        case 'e':
+        else if(op.compare("-e") == 0)
             status = extrair(argv[2]);
-            break;
-        default:
-            cout<<"Formato Incorreto: Descrição do programa"<<endl;
-            status = 1;
+        else
+        {
+            info();
+            status = 3;
         }
     }
     return status;
